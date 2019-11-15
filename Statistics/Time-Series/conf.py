ed4f5e004bb85065950abc7844c1067d648758a1
wklchris <wklchris@hotmail.com>
2019-11-15 08:25:03 -0800

    Update: stats/timeseries Intro chapter.

---
+++ Statistics/Time-Series/conf.py
@@ -0,0 +1,22 @@
+project = '时间序列浅谈'
+copyright = '2019, wklchris'
+author = 'wklchris'
+
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+    'sphinx_rtd_theme'
+]
+
+language = 'zh_CN'
+
+exclude_patterns = [
+    '_build', 'Thumbs.db', '.DS_Store',
+    '**.ipynb_checkpoints'
+]
+
+html_theme = "sphinx_rtd_theme"
+templates_path = ['../../_global/_templates']
+
+html_static_path = ['../../_global/_static']
+html_css_files = ['style.css']
